ac658987e3d7e37b59fdf22783fd5b5080256de5
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2017-10-30 19:58:03 -0700

    add kitten images

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Click on the kitty!</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <img />
+        </p>
+        <p>
+          Kitten images sourced from <a href="https://www.instagram.com/riepoyonn/">@riepoyonn</a>
         </p>
       </div>
     );
