73998e3fc2efd3219975a0349a94aeec7cf0facc
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2017-11-02 08:00:59 -0700

    add better kitten handling

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import './App.css';
 import KittenImage from './components/KittenImage'
 
 class App extends Component {
-  state = { mews: 0 }
-  handleClick = () => this.setState(({ mews }) => ({ mews: mews + 1 }))
+  state = { mews: 0, kittenLevel: 0, upgradeAvailable: false, upgradeCost: 10 }
+  handleKittenClick = () => this.setState(({ mews, upgradeCost }) => ({
+    mews: mews + 1,
+    upgradeAvailable: mews + 1 >= upgradeCost,
+  }))
+
+  upgradeKitten = () => {
+    this.setState(({ mews, upgradeCost, kittenLevel }) => ({
+      mews: mews - upgradeCost,
+      upgradeCost: upgradeCost * 2,
+      upgradeAvailable: mews - upgradeCost >= upgradeCost * 2,
+      kittenLevel: kittenLevel + 1,
+    }))
+  }
+
   render() {
-    const { mews } = this.state
+    const { mews, upgradeAvailable, kittenLevel, upgradeCost } = this.state
     return (
       <div className="App">
         <header className="App-header">
@@ -14,8 +27,9 @@ class App extends Component {
           <p>Mews: {mews}</p>
         </header>
         <p className="App-intro">
-          <KittenImage onClick={this.handleClick} />
+          <KittenImage onClick={this.handleKittenClick} kittenLevel={kittenLevel} />
         </p>
+        <button disabled={!upgradeAvailable} onClick={this.upgradeKitten}>Upgrade kitten for {upgradeCost} mews</button>
         <p>
           Kitten images sourced from <a href="https://www.instagram.com/riepoyonn/">@riepoyonn</a>
         </p>
--- src/components/KittenImage.js
+++ src/components/KittenImage.js
@@ -9,13 +9,46 @@ import kitten07 from './../assets/kitten07.jpg'
 import kitten08 from './../assets/kitten08.jpg'
 import kitten09 from './../assets/kitten09.jpg'
 import kitten10 from './../assets/kitten10.png'
-import kitten11 from './../assets/kitten06.jpg'
+import kitten11 from './../assets/kitten11.jpg'
 
+const getKittenImage = kittenLevel => {
+  if (kittenLevel < 1) {
+    return kitten01
+  }
+  if (kittenLevel < 2) {
+    return kitten02
+  }
+  if (kittenLevel < 3) {
+    return kitten03
+  }
+  if (kittenLevel < 4) {
+    return kitten04
+  }
+  if (kittenLevel < 5) {
+    return kitten05
+  }
+  if (kittenLevel < 6) {
+    return kitten06
+  }
+  if (kittenLevel < 7) {
+    return kitten07
+  }
+  if (kittenLevel < 8) {
+    return kitten08
+  }
+  if (kittenLevel < 9) {
+    return kitten09
+  }
+  if (kittenLevel < 10) {
+    return kitten10
+  }
+  return kitten11
+}
 
 class KittenImage extends Component {
   render() {
-    const { onClick } = this.props
-    return <img src={kitten01} onClick={onClick} className="kitten-image" alt="Kitten" />
+    const { onClick, kittenLevel } = this.props
+    return <img src={getKittenImage(kittenLevel)} onClick={onClick} className="kitten-image" alt="Kitten" />
   }
 }
 
