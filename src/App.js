73991f3b5bdb946b15b8ef3af3bf3ac269cd0621
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2017-11-10 09:17:05 -0800

    revert

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Click on the kitty!</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          <img />
-        </p>
-        <p>
-          Kitten images sourced from <a href="https://www.instagram.com/riepoyonn/">@riepoyonn</a>
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
