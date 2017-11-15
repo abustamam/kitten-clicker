3d409f77a6df90fd103510f62af9e1e55426f32a
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2017-11-15 09:13:54 -0800

    add state to counter

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-class Mycomponent extends Component {
+class Counter extends Component {
+  state = {
+    count: 0,
+  }
+
+  handleClick() {
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+
   render() {
-    return <div className="Mycomponent">Hi i am my component</div>
+    const { count } = this.state
+    return (
+      <div className="Mycomponent" onClick={this.handleClick.bind(this)}>
+        Hi i am my component {count}
+      </div>
+    )
   }
 }
 
@@ -19,9 +34,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and load to reload.
         </p>
-        <Mycomponent />
-        <Mycomponent />
-        <Mycomponent />
+        <Counter />
+        <Counter />
+        <Counter />
       </div>
     )
   }
